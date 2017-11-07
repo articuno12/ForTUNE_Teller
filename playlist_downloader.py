672b9756021f55225a6494e3362c8a8dada33b26
Kritika <kritikagr12@gmail.com>
2017-11-08 04:29:36 +0530

    playlist downloader

---
+++ playlist_downloader.py
@@ -0,0 +1,4 @@
+import os
+#os.system("youtube-dl -citk FORMAT https://www.youtube.com/playlist?list=PLvFsG9gYFxY9zTBhcFmMcYa3zYfQz7P7F")
+os.system("youtube-dl -cit --extract-audio --audio-format mp3 https://www.youtube.com/playlist?list=PLB3k5t7CLOoQ2dICJ-iDr_iZW7LUMC3-w")
+# os.system("rm *.mp4")
