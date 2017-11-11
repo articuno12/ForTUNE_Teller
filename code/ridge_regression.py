a4934207111476e4dc8540831cd5fd1e0f302ec3
SumaReddy98 <suma.4941@gmail.com>
2017-11-11 10:14:06 +0000

    Added accuracy score

--- code/ridge_regression.py
+++ code/ridge_regression.py
@@ -15,35 +15,44 @@ import random
 
 data = pd.read_csv("../dataset/file_20_feat.csv")
 #X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-X_1 = data.drop("data_set_id",axis=1)
-drop_label = X_1.drop("20",axis=1)
-X = drop_label.drop("21",axis=1)
+#X_1 = data.drop("data_set_id",axis=1)
+#drop_label = X_1.drop("20",axis=1)
+#X = drop_label.drop("21",axis=1)
+#Y = data["20"]
+#seed = 7
+#test_size = 0.25
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+#print X_train, X_test, y_train, y_test
+
 Y = data["20"]
 seed = 7
 test_size = 0.25
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-#print X_train, X_test, y_train, y_test
+X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=test_size, random_state=seed)
+X_t = X_train.drop("20",axis =1)
+X_tt= X_t.drop("21",axis =1)
+X_TRAIN = X_tt.drop("data_set_id",axis =1)
+X_t1 = X_test.drop("20",axis =1)
+X_tEST= X_t1.drop("21",axis =1)
+X_TEST = X_tEST.drop("data_set_id",axis =1)
+y_labels = X_test["21"]
 
-ridge_regress = linear_model.Ridge(alpha=10000)
-ridge_regress.fit(x_train, y_train)
+ridge_regress = linear_model.Ridge(alpha=0.00001)
+ridge_regress.fit(X_TRAIN, y_train)
 #print ridge_regress
-unrounded_y_pred = ridge_regress.predict(x_test)
+unrounded_y_pred = ridge_regress.predict(X_TEST)
 y_pred = np.round(unrounded_y_pred)
 #print y_pred
 var = y_test - y_pred
 inc = sum(abs(var))
-flag = inc/len(x_train)
+flag = inc/len(X_TRAIN)
+print "flag = " 
+print flag
 final_acc = 1 - flag
 for i in range(len(y_pred)):
-    print y_pred[i]
-#print final_acc
-#for i in range(len(y_test)):
-#	print abs(y_test)
+    print abs(y_pred[i])
 label = []
 
 for i in range(len(y_pred)):
-	#print "ith value"
-	#print i
 	if(y_pred[i]<=20000):
 		label.append(1)
 	if(y_pred[i]>20000 and y_pred[i]<=60000):
@@ -58,9 +67,7 @@ for i in range(len(y_pred)):
 print "labels"
 
 for i in range(len(label)):
-	print label[i]
-print "Mean_squared_error"
-print(mean_squared_error(y_test,y_pred))
-print "Mean absolute error"
-print(mean_absolute_error(y_test,y_pred))
+	print i, label[i], y_labels.values[i]
 
+acc_score = sklearn.metrics.accuracy_score(y_labels, label)
+print acc_score
