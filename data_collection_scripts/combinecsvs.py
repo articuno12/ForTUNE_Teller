987190d3d42aa516eebee3aa39fd00370f87ceeb
Kritika Agrawal <articuno12@users.noreply.github.com>
2017-11-25 00:07:00 +0530

    code to combine csv files after first cleaning

---
+++ data_collection_scripts/combinecsvs.py
@@ -0,0 +1,19 @@
+import pandas as  pd
+import sys
+
+
+d1 = pd.read_csv(sys.argv[1])
+d2 = pd.read_csv(sys.argv[2])
+d3 = pd.read_csv(sys.argv[3])
+d4 = pd.read_csv(sys.argv[4])
+d5 = pd.read_csv(sys.argv[5])
+# d6 = pd.read_csv(sys.argv[1])
+#merge datasets
+frames = [d1, d2, d3, d4,d5]
+result = pd.concat(frames)
+print result.shape
+
+#remove songs for which we couldnt find viewcount
+valid_result = result[~(result["viewCount"]==0)]
+print valid_result.shape
+valid_result.to_csv(sys.argv[6])
