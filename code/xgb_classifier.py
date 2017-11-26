5c79ca94ee1956048223daf84fadac8ddb35df85
articuno12 <kritikagr12@gmail.com>
2017-11-26 08:29:59 +0530

    training xgb

--- code/xgb_classifier.py
+++ code/xgb_classifier.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import GridSearchCV   #Perforing grid search
 from sklearn.metrics import mean_squared_error,mean_absolute_error,accuracy_score,f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("../dataset/final_debut_20.csv")
+data = pd.read_csv("../dataset/final_20.csv")
 print data.shape
 
 Y = data["label"]
@@ -43,8 +43,8 @@ xgb_params = {
 model = xgb.XGBClassifier(learning_rate = xgb_params['learning_rate'], max_depth = 10,
 						n_estimators = 800, silent = xgb_params['silent'],
 						objective = xgb_params['objective'],
-						min_child_weight = 0.5, gamma = 0.01 ,
-						subsample = 0.8, colsample_bytree = 1 )
+						min_child_weight = 0.8, gamma = 0.05 ,
+						subsample = 0.5, colsample_bytree = 0.8)
 model.fit(X_TRAIN,y_train)
 y_predcited = model.predict(X_TEST)
 print accuracy_score(y_test,y_predcited)
