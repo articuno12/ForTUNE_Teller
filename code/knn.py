5c5ae49b3c7c5e5204070cbf181333e10a879ca0
n5596 <nfatima5596@gmail.com>
2017-11-26 17:35:11 +0530

    kNN classifier for debut dataset with 50 features

--- code/knn.py
+++ code/knn.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score,f1_score
+from sklearn.metrics import accuracy_score,f1_score,confusion_matrix
 from sklearn.grid_search import GridSearchCV   #Perforing grid search
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 import os
@@ -14,24 +14,27 @@ from math import *
 import random
 
 #data = np.genfromtxt('/home/naila/5thSemester/SMAI/Codes/file_20_feat.csv',delimiter = ',')
-data = pd.read_csv("file_20_feat.csv")
+data = pd.read_csv("final_debut_50.csv")
 
-Y = data["21"]
-#print Y[1]
+Y = data["views"]
 
 seed = 7
 test_size = 0.25
 X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=test_size, random_state=seed)
+#print X_train.shape, X_test.shape, y_train.shape,y_test.shape
+X_t = X_train.drop("views",axis =1)
+#X_t = X_t.drop("data_set_id", axis=1)
+X_tt= X_t.drop("label",axis =1)
+X_TRAIN = X_tt.drop("ID",axis =1)
+X_t1 = X_test.drop("views",axis =1)
+#X_t1 = X_t1.drop("data_set_id", axis=1)
+X_tEST= X_t1.drop("label",axis =1)
+X_TEST = X_tEST.drop("ID",axis =1)
 
-X_t = X_train.drop("20",axis =1)
-X_tt= X_t.drop("21",axis =1)
-X_TRAIN = X_tt.drop("data_set_id",axis =1)
-X_t1 = X_test.drop("20",axis =1)
-X_tEST= X_t1.drop("21",axis =1)
-X_TEST = X_tEST.drop("data_set_id",axis =1)
-y_labels = X_test["21"]
+y_train = X_train["label"]
+y_labels = X_test["label"]
 
-n_neighbors = 5
+n_neighbors = 7
 
 model = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
 model.fit(X_TRAIN, y_train)
@@ -42,5 +45,7 @@ predictions = model.predict(X_TEST)
 #    print abs(predictions[i])
 
 acc_score = sklearn.metrics.accuracy_score(y_labels, predictions)
-fscore = sklearn.metrics.accuracy_score(y_labels, predictions)
-print acc_score,fscore
+cm = sklearn.metrics.confusion_matrix(y_labels, predictions)
+print acc_score
+print cm
+#print y_train
