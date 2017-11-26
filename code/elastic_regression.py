552067d4365d821bedb319338f417e113ed634a3
SumaReddy98 <suma.4941@gmail.com>
2017-11-26 22:12:25 +0530

    Elastic regression with final changes

--- code/elastic_regression.py
+++ code/elastic_regression.py
@@ -13,28 +13,24 @@ from numpy import genfromtxt
 from math import *
 import random
 
-data = pd.read_csv("../dataset/file_20_feat.csv")
-#X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-#X_1 = data.drop("data_set_id",axis=1)
-#drop_label = X_1.drop("20",axis=1)
-#X = drop_label.drop("21",axis=1)
-#Y = data["20"]
-#seed = 7
-#test_size = 0.25
-#x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-#print X_train, X_test, y_train, y_test
-
-Y = data["20"]
+data = pd.read_csv(sys.argv[1])
+Y = data["views"]
 seed = 7
 test_size = 0.25
 X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=test_size, random_state=seed)
-X_t = X_train.drop("20",axis =1)
-X_tt= X_t.drop("21",axis =1)
-X_TRAIN = X_tt.drop("data_set_id",axis =1)
-X_t1 = X_test.drop("20",axis =1)
-X_tEST= X_t1.drop("21",axis =1)
-X_TEST = X_tEST.drop("data_set_id",axis =1)
-y_labels = X_test["21"]
+#print X_train.shape, X_test.shape, y_train.shape,y_test.shape
+X_t = X_train.drop("views", axis =1)
+X_tt= X_t.drop("label", axis=1)
+X_TRAIN= X_tt.drop("ID", axis=1)
+#X_TRAIN = X_ttt.drop("ID", axis=1)
+X_t1 = X_test.drop("views", axis=1)
+X_tEST= X_t1.drop("label", axis=1)
+X_TEST= X_tEST.drop("ID", axis=1)
+#X_TEST = X_ts.drop("ID", axis=1)
+# x_test = X_TEST["1","2","7","14","18"]
+# x_train = X_TRAIN["1","2","7","14","18"]
+y_labels = X_test["label"]
+
 
 ridge_regress = linear_model.ElasticNet(alpha=0.1,l1_ratio=0.5)
 ridge_regress.fit(X_TRAIN, y_train)
@@ -45,11 +41,11 @@ y_pred = np.round(unrounded_y_pred)
 var = y_test - y_pred
 inc = sum(abs(var))
 flag = inc/len(X_TRAIN)
-#print "flag = " 
+#print "flag = "
 #print flag
 final_acc = 1 - flag
-#for i in range(len(y_pred)):
-#    print abs(y_pred[i])
+for i in range(len(y_pred)):
+    print abs(y_pred[i])
 label = []
 
 for i in range(len(y_pred)):
@@ -59,15 +55,18 @@ for i in range(len(y_pred)):
 		label.append(2)
 	if(y_pred[i]>60000 and y_pred[i]<=300000):
 		label.append(3)
-	if(y_pred[i]>300000 and y_pred[i]<=2000000):
+	if(y_pred[i]>300000 and y_pred[i]<=3000000):
 		label.append(4)
-	if(y_pred[i]>2000000 and y_pred[i]<=1900000000):
+	if(y_pred[i]>3000000):
 		label.append(5)
 
-#print "labels"
+print "labels"
 
 for i in range(len(label)):
 	print label[i]
 
 acc_score = sklearn.metrics.accuracy_score(y_labels, label)
 print acc_score
+cm = sklearn.metrics.confusion_matrix(y_labels, label)
+print cm
+
